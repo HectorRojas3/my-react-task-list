c000fdcdf9f960e32a15a3ffc0b5a5bff627c873
rojashd <135187158+HectorRojas3@users.noreply.github.com>
2023-08-13 23:52:27 -0600

    primera parte del componente task

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
 import Header from './components/header'
+import Task from './components/Task'
 
 function App() {
+  
+  const tasks = [
+    {id: 1, name: 'Ver repeticion de React intro', done: true},
+    {id: 1, name: 'Presentar proyecto integrador', done: false},
+    {id: 1, name: 'Leer presentacion Hooks 1', done: false},
+  ];
+  
+  const handleToggleTask = (taskId) => {
+
+  };
+  
   return (
     <>
       <Header text={"TO-DO List"}/>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onToggle={handleToggleTask} />
+      ))}
     </>
   )
 }
